e8f28752dda2970335beeb014f402705d81ea0da
Renee Nordholm <reneenordholm@gmail.com>
2019-03-25 11:48:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,5 @@
-def prime?(value, generator = Prime::Generator23.new)
-  raise ArgumentError, "Expected a prime generator, got #{generator}" unless generator.respond_to? :each
-  raise ArgumentError, "Expected an integer, got #{value}" unless value.respond_to?(:integer?) && value.integer?
-  return false if value < 2
-  generator.each do |num|
-    q,r = value.divmod num
-    return true if q < num
-    return false if r == 0
-  end
+def prime?(num)
+  return false if num <= 1
+
+   (2..num-1).none? { |div| num % div == 0 }
 end
